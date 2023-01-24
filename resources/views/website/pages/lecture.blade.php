3408b681e67cdfb412f4c159b6b82f098ec433fa
scp-sabuj <60912036+scp-sabuj@users.noreply.github.com>
2023-01-24 14:36:36 +0600

    changes

--- resources/views/website/pages/lecture.blade.php
+++ resources/views/website/pages/lecture.blade.php
@@ -27,7 +27,7 @@
                     </div>
                 </div>
             @elseif ($data->cover_type == 4)
-                <iframe src="{{ asset('storage/'.$data->file)}}" title="{{ $data->title }}" height="100%" width="100%" />
+                <iframe src="{{ asset('storage/'.$data->file)}}" title="{{ $data->title }}" height="100%" width="100%"> </iframe>
             @else
             <div class=" lecture-cover">
                 @php
